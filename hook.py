7b411c566bd19e88789092c967c76f2660d7deb5
Pedro Souza <pedrosouzadz6@pm.me>
2025-11-14 16:39:32 +1300

    feat: add MouseHook class for low-level mouse event handling
    Implement Windows API hooking functionality to monitor mouse events using ctypes

--- hook.py
+++ hook.py
@@ -12,3 +12,20 @@ class Constants:
     WM_LBUTTONUP = 0x0202
     MOUSEEVENTF_LEFTDOWN = 0x0002
     MOUSEEVENTF_LEFTUP = 0x0004
+
+class MouseHook:
+    user32 = ctypes.windll.user32
+    kernel32 = ctypes.windll.kernel32
+
+    LowLevelMouseProc = ctypes.WINFUNCTYPE(ctypes.c_int, ctypes.c_int, wintypes.WPARAM, wintypes.LPARAM)
+
+    user32.SetWindowsHookExW.argtypes = [ctypes.c_int, LowLevelMouseProc, wintypes.HINSTANCE, wintypes.DWORD]
+    user32.SetWindowsHookExW.restype = ctypes.c_void_p
+    user32.CallNextHookEx.argtypes = [ctypes.c_void_p, ctypes.c_int, wintypes.WPARAM, wintypes.LPARAM]
+    user32.CallNextHookEx.restype = ctypes.c_int
+    user32.UnhookWindowsHookEx.argtypes = [ctypes.c_void_p]
+    user32.UnhookWindowsHookEx.restype = wintypes.BOOL
+    user32.GetMessageW.argtypes = [ctypes.POINTER(wintypes.MSG), wintypes.HWND, wintypes.UINT, wintypes.UINT]
+    user32.GetMessageW.restype = wintypes.BOOL
+    user32.TranslateMessage.argtypes = [ctypes.POINTER(wintypes.MSG)]
+    user32.DispatchMessageW.argtypes = [ctypes.POINTER(wintypes.MSG)]
